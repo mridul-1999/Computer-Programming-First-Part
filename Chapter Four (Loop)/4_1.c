d10dc65da70694e9421a802252f9978ac784fd1e
Saif Mahmud <67528956+mridul-1999@users.noreply.github.com>
2023-01-07 21:49:36 +0600

    Loop
    This File While/For Loop Program.

---
+++ Chapter Four (Loop)/4_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("1 \n");
+    printf("2 \n");
+    printf("3 \n");
+    printf("4 \n");
+    printf("5 \n");
+    printf("6 \n");
+    printf("7 \n");
+    printf("8 \n");
+    printf("9 \n");
+    printf("10 \n");
+
+    //printf(" 1 \n 2 \n 3 \n 4 \n 5 \n 6\n 7 \n 8 \n 9 \n 10 ");
+    return 0;
+}
